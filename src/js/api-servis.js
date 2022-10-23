34560ca8f623722791dd39ee4e0a56901db14330
Артем Гладуш <m1hommage5008@gmail.com>
2022-10-23 12:15:23 +0300

    add files

---
+++ src/js/api-servis.js
@@ -0,0 +1,42 @@
+import Axios from 'axios';
+const API_KEY = '28133601-50b96a0b59b26f277c741bb78';
+const API_URL = 'https://pixabay.com/api/';
+
+export default class ImagesApiService {
+	constructor() {
+   this.searchQuery = " ";
+   this.page = 1;
+   this.per_page = 40;
+	};
+
+	async fetchImages() {
+   	const options = new URLSearchParams({
+      key: API_KEY,
+      q: this.query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: this.per_page,
+      page: this.page
+   });
+   	const url = `${API_URL}?${options}`;
+   	const response = await Axios.get(url);
+   	this.totalHits = response.data.totalHits;
+		console.log(this.totalHits);
+   	return response.data.hits;
+	}
+
+	incrementPage() {
+   	this.page += 1;
+	}
+	resetPage() {
+   	this.page = 1;
+	}
+
+	get query() {
+   	return this.searchQuery;
+	}
+	set query(newQuery) {
+   	this.searchQuery = newQuery;
+	}
+};
